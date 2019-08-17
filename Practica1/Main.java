7b36d004dc109f3e350a6af61b1c1637a9c1b554
NerakCM <53309112+NerakCM@users.noreply.github.com>
2019-08-17 14:34:08 -0500

    Update Main.java

--- Practica1/Main.java
+++ Practica1/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main (String[]args){
-	System.out.println("java version \"1.8.0_221\"");
-        System.out.println("Java(TM) SE Runtime Environment (build 1.8.0_221-b11)");
-	System.out.println("Java HotSpot(TM) 64-Bit Server VM (build 25.221-b11,0mixed mode)\n"); 
+	System.out.println("openjdk version \"11.0.4\" 2019-07-16");
+        System.out.println("OpenJDK Runtime Environment (build 11.0.4+11-post-Ubuntu-1ubuntu218.04.3)");
+	System.out.println("OpenJDK 64-Bit Server VM (build 11.0.4+11-post-Ubuntu-1ubuntu218.04.3, mixed mode, sharing)\n"); 
         System.out.println("Linux nerak-HP-240-G3-Notebook-PC 5.0.0-25-generic #26~18.04.1-Ubuntu SMP Thu Aug 1 13:51:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux");
 
     }
