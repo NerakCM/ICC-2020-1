5fef2ca8ed387b37e2776fcb1e813d33998844c1
NerakCM <karencm@ciecias.unam.mx>
2019-08-17 18:15:54 -0500

    subir

--- Practica1/Main.java
+++ Practica1/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main (String[]args){
-	System.out.println("Hola gg");
-	System.out.println("java version  1.8.0_221");
+	System.out.println("java version \"1.8.0_221\"");
         System.out.println("Java(TM) SE Runtime Environment (build 1.8.0_221-b11)");
-	System.out.println("Java HotSpot(TM) 64-Bit Server VM (build 25.221-b11,0mixed mode)");
+	System.out.println("Java HotSpot(TM) 64-Bit Server VM (build 25.221-b11,0mixed mode)\n"); 
+System.out.println("Linux nerak-HP-240-G3-Notebook-PC 5.0.0-25-generic #26~18.04.1-Ubuntu SMP Thu Aug 1 13:51:02 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux");
+
     }
 }
 
